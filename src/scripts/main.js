2fe4ae1b390d12410f66f804fce250edbda89efa
Вася <pankulich654g@mail.com>
2025-03-31 23:36:12 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,42 @@ const food = {
   },
 };
 
+const wrapper = document.createElement('ul');
+
+document.body.prepend(wrapper);
+wrapper.setAttribute('id', 'tree');
+
 const tree = document.querySelector('#tree');
 
 function createTree(element, data) {
-  // WRITE YOUR CODE HERE
-}
+  const ul = document.createElement('ul');
 
+  element.append(ul);п
+
+  for (const key in data) {
+    const values = data[key];
+
+    if (Object.keys(values).length !== 0) {
+      createTree(element, values);
+    } else {
+      const li = document.createElement('li');
+
+      li.innerHTML = key;
+      ul.append(li);
+    }
+  }
+}
 createTree(tree, food);
+
+// if(data) {
+//   createTree(element, data);
+// } else {
+//   const ul = document.createElement('ul')
+//   document.body.prepend(ul);
+
+//   for (let key in data) {
+//     const li = document.createElement('li');
+//     li.innerHTML = key;
+//     wrapper.append(li)
+//   }
+// }
